3b341565fec7e849b75c965a7c6092cbacdc2635
Yvonne Muriuki <yvonnemuriuki01@gmail.com>
2024-05-20 22:59:07 +0300

    complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
-def greet(name):
+def greet(name = "Naureen"):
+    print(f"Hello, {name}!")
     pass
 
 def greet_with_default(name="programmer"):
+
+    print(f"Hello, {name}!")
     pass
 
 def add(num1, num2):
+    sum = (num1+num2)
+    print(sum)
+    return sum
+    add(1,2)
     pass
 
 def halve(number):
+    half= number/2
+    return half
     pass
+
